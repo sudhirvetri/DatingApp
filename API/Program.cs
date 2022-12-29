95d6f8cb8f2a3a1e126dfc69fe98652f30f2dae4
sudhir <sudhirvetri@gmail.com>
2022-12-29 12:29:58 +0530

    Added Angular components

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,24 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 //using the builder we can inject the datacontext class here making all the methods used in that class can be injected here.
 builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.WithOrigins("https://localhost:4200").AllowAnyHeader().AllowAnyMethod());
 app.MapControllers();
 
 app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import{HttpClient, HttpClientModule} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',  
+  templateUrl:'../template.html',
+  styleUrls: ['./app.component.css'],
+   
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http : HttpClient){}
+  
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/users").subscribe({
+      next: output =>this.users=output,
+      error: error=>console.log(error),
+      complete:() =>console.log('Request has completed')      
+    })
+  }
+}
